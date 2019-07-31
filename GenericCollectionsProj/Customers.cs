3b34da41a3bfa87501bb4fdb0a82a946a3025c2e
Marianne Linardos <mlinardos.edu@gmail.com>
2019-07-31 17:14:22 -0400

    added dictionalry class

---
+++ GenericCollectionsProj/Customers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionsProj {
+    class Customers {
+
+        List<Customer> customerList = new List<Customer>();
+
+        public void LoadCustomers() {
+            var cust1 = new Customer { Name = "P&G", State = "OH" };      // way to initialize the properties of class without having to create constructors for each customer
+            var cust2 = new Customer { Name = "Microsoft", State = "WA" };
+            var cust3 = new Customer { Name = "Target", State = "MN" };
+
+            customerList.Add(cust1);        // Adding to our collection
+            customerList.Add(cust2);
+            customerList.Add(cust3);
+        }
+
+        public void PrintCustomers() {
+            foreach (var cust in customerList) {// last perimeter is always collection and already exists; the other 3 sections in parens are created on the fly; can't use 'customer' because it's an instance of a class
+                Console.WriteLine($"Customer name is {cust.name} and home state is {cust.State} ;
+
+        }
+
+    }
+
+    public class Customer {         // add singular 'Customer' class after closing curly brace of Customers
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
--- GenericCollectionsProj/Program.cs
+++ GenericCollectionsProj/Program.cs
@@ -1,11 +1,73 @@
 ﻿using System;
-
+using System.Collections.Generic;
+// vid 7/31/19 p3 1:29 / Reference GD   &   p4
 namespace GenericCollectionsProj {
     class Program {
         static void Main(string[] args) {
-           
 
+            var customers = new /*List<Customer>*/Customers();
+
+            //List<string> custs = new List<string>();
+            customers.LoadCustomers();
+            //customers.PrintCustomers();
+
+
+            //public void PrintCustomers() {
+            //    foreach (var cust in customerList) {// last perimeter is always collection and already exists; the other 3 sections in parens are created on the fly; can't use 'customer' because it's an instance of a class
+            //   Console.WriteLine(cust.name};
+
+            //}
+
+        }// add close to main
+         //static void run() { // create run method with only opening brace
+         //    var rnd = new Random();
+
+        //    List<int> Frames = new List<int>();
+
+        //    int i = 0;
+        //    //int RegRnd = new Random(21);
+
+        //    for(i=0; i<10; i++) {
+        //        var score = new Random();
+        //        rnd.Next(11);
+        //if(rnd = )
+        /*  foreach() */      // this will be after the for loop;  it will reference the collection that is created during the for loop
+    }
+
+
+
+
+
+
+
+
+
+
+
+            ////List<string> names = new List<string> {
+            ////    "Greg", "Marianne", "Brailee"
+            ////};
+
+
+            //List<string> names = new List<string>()
+
+            //// Generic Collections
+            //var integers = new List<int>(); // select first suggest fix for rsquig 'Using ...'
+
+            //integers.Add(7);        // generic collection of 3 ints; used addmethod; void- returns nothing
+            //integers.Add(4);
+            //integers.Add(13);
+
+            //integers.Remove(4);
+
+            //integers.AddRange(new int[] { 4, 44, 444, 4444 });
+
+            //integers[3] = 99;
+
+            //foreach(var i in integers) {
+            //    Console.WriteLine($"i = {i}");
+            //}
 
         }
-    }
-}
+    
+
